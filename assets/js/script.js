90b39bf3f2177579d7bd34fae5a4bf837a8b6929
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-10-14 19:13:22 +0600

    Putting the comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
+// Clock container div
 let ele = document.createElement('div');
 
 function liveTime() {
     let date = new Date();
-    let time = date.getTime();
     let seconds = date.getSeconds();
     let minutes = date.getMinutes();
     let hours = date.getHours();
     let formate = 'AM';
 
+    // Adding '0' when the digit is single
     seconds = seconds < 10 ? '0' + seconds : seconds;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     hours = hours < 10 ? '0' + hours : hours;
@@ -20,10 +21,11 @@ function liveTime() {
         formate = 'PM';
     }
 
+    // Inserting the time into the div and appending the div into the main body
     ele.innerText = hours + ' : ' + minutes + ' : ' + seconds + ' ' + formate;
     document.body.appendChild(ele);
 }
 
+// Calling the function 
 liveTime();
-
 setInterval(liveTime, 1000)
