3ff1ea9f680fc3c6dfaeeba5dd8597ef842a6700
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-10-14 12:09:51 +0600

    Live clock created using vanilla JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+let ele = document.createElement('div');
+
+function liveTime() {
+    let date = new Date();
+    let time = date.getTime();
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let formate = 'AM';
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    hours = hours < 10 ? '0' + hours : hours;
+
+    if(hours == 0) {
+        hours = 12;
+    }
+    if(hours > 12) {
+        hours = hours - 12;
+        formate = 'PM';
+    }
+
+    ele.innerText = hours + ' : ' + minutes + ' : ' + seconds + ' ' + formate;
+    document.body.appendChild(ele);
+    console.log(hours + ' / ' + minutes + ' / ' + seconds);
+}
+
+liveTime();
+
+setInterval(liveTime, 1000)
